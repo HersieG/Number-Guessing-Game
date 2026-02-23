5d862deb0dc146fee132544d5e1e70e0afd9b8b5
HersieG <hersiedgonzalez@gmail.com>
2026-02-23 11:41:11 -0500

    fix: fixed random number pick

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ playAgain.onclick = function () {
   playAgain.style.display = "none";
   message.textContent = "";
   guess.value = "";
-  number = Math.floor(Math.random() * 10) + 1;
+  correctNumber = Math.floor(Math.random() * 10) + 1;
 };
